97528e2fbd32e4142c1d62063bea0f5996188293
JasonBats <46994866+JasonBats@users.noreply.github.com>
2024-04-26 11:17:11 +0200

    Added can_be_contacted and ca_be_shared methods on User model.

--- authentication/models.py
+++ authentication/models.py
@@ -4,7 +4,16 @@ from SoftDeskSupport import utils
 
 
 class User(AbstractUser):
-    can_be_contacted = models.BooleanField(null=False, default=False)
-    can_be_shared = models.BooleanField(null=False, default=False)
+
     birth_date = models.DateField(null=False, blank=False)
-    age = utils.get_user_age(birth_date)
+    age = models.IntegerField(null=False)
+
+    can_be_contacted = models.BooleanField(
+        null=False,
+        default=False,
+        verbose_name="J'autorise SoftDeskSupport à me contacter"
+    )
+    can_be_shared = models.BooleanField(
+        null=False,
+        default=False
+    )
