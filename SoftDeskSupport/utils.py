277a28ef8787889dc7a7d7345c8e63fccb6360e5
JasonBats <46994866+JasonBats@users.noreply.github.com>
2024-04-26 11:20:13 +0200

    get_user_age method (from birth_date) and define_can_be_shared method from determined age.

--- SoftDeskSupport/utils.py
+++ SoftDeskSupport/utils.py
@@ -3,4 +3,13 @@ import datetime
 
 def get_user_age(birth_date):
     today = datetime.datetime.today()
-    print(today, birth_date)
+    age = (today.year - birth_date.year
+           - ((today.month, today.day) < (birth_date.month, birth_date.day)))
+    return age
+
+
+def define_can_be_shared(age):
+    if age >= 15:
+        return True
+    else:
+        return False
