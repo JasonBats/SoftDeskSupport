3e171941bf2a419b4129dcdb88c7eb67769e1a76
JasonBats <46994866+JasonBats@users.noreply.github.com>
2024-04-26 11:21:50 +0200

    Signup method introducing get_user_age and can_be_shared methods.

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from . import forms
 from django.contrib.auth import login
 from django.conf import settings
+from SoftDeskSupport.utils import get_user_age, define_can_be_shared
 
 
 def signup(request):
@@ -9,9 +10,10 @@ def signup(request):
     if request.method == 'POST':
         form = forms.SignupForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save(commit=False)
+            user.age = get_user_age(user.birth_date)
+            user.can_be_shared = define_can_be_shared(user.age)
+            user.save()
             login(request, user)
             return redirect(settings.LOGIN_REDIRECT_URL)
     return render(request, 'authentication/signup.html', context={'form': form})
-
-
