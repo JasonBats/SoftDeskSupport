0260b922a144c2f00d0e088f15943bb0a0701527
JasonBats <46994866+JasonBats@users.noreply.github.com>
2024-05-07 17:56:06 +0200

    Required fields added for POST methods.

--- SoftDeskApp/serializers.py
+++ SoftDeskApp/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from SoftDeskApp.models import Project, Issue, Comment
+from SoftDeskApp.models import Project, Issue, Comment, Contributor
+from authentication.models import User
 
 
 class ProjectDetailSerializer(serializers.ModelSerializer):
@@ -23,7 +24,8 @@ class ProjectListSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'description', 'author', 'number_of_issues']
 
     def get_number_of_issues(self, instance):
-        number_of_issues_queryset = Issue.objects.filter(project=instance).count()
+        number_of_issues_queryset = Issue.objects.filter(
+            project=instance).count()
         return number_of_issues_queryset
 
 
@@ -44,7 +46,8 @@ class IssueDetailSerializer(serializers.ModelSerializer):
 class IssueListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Issue
-        fields = ['id', 'name', 'description', 'author', 'assigned_to']
+        fields = ['id', 'name', 'description',
+                  'author', 'assigned_to', 'project']
 
 
 class CommentDetailSerializer(serializers.ModelSerializer):
@@ -67,9 +70,38 @@ class CommentListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ['id', 'issue']
+        fields = ['id', 'issue', 'description']
 
     def get_issue(self, instance):
         queryset = instance.issue
         serializer = IssueListSerializer(queryset, many=False)
         return serializer.data
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'birth_date', 'age', 'password']
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def validate_age(self, value):
+        if value < 15:
+            raise serializers.ValidationError('Vous n\'avez pas'
+                                              ' l\'age requis (15 ans)')
+
+
+class ContributorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contributor
+        fields = '__all__'
+
+
+class ContributorDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contributor
+        fields = '__all__'
