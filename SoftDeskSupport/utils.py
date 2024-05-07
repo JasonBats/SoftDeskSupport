a821d86d852cc2c85abe390d6625363931d5d8bd
JasonBats <46994866+JasonBats@users.noreply.github.com>
2024-05-07 18:00:49 +0200

    Deserialized dates for get_user_age function.

--- SoftDeskSupport/utils.py
+++ SoftDeskSupport/utils.py
@@ -3,12 +3,15 @@ import datetime
 
 def get_user_age(birth_date):
     today = datetime.datetime.today()
-    age = (today.year - birth_date.year
-           - ((today.month, today.day) < (birth_date.month, birth_date.day)))
+    birth_date_year = int(birth_date[0:4])
+    birth_date_month = int(birth_date[5:7])
+    birth_date_day = int(birth_date[8:10])
+    age = (today.year - birth_date_year
+           - ((today.month, today.day) < (birth_date_month, birth_date_day)))
     return age
 
 
-def define_can_be_shared(age):
+def define_can_be_signed_up(age):
     if age >= 15:
         return True
     else:
