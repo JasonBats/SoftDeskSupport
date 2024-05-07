81485e084ade1047d9d4145b14a047c223bcf442
JasonBats <46994866+JasonBats@users.noreply.github.com>
2024-05-07 17:59:12 +0200

    Updated views

--- SoftDeskSupport/urls.py
+++ SoftDeskSupport/urls.py
@@ -2,35 +2,32 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from authentication.views import signup
-from SoftDeskApp.views import (project_creation_view,
-                               issue_creation_view,
-                               comment_creation_view,
-                               homepage,
-                               ProjectViewSet,
+from SoftDeskApp.views import (ProjectViewSet,
                                IssueViewSet,
                                CommentViewSet,
-                               AdminProjectViewSet,
                                StaffIssueViewSet,
+                               UserListViewSet,
+                               ContributorViewSet
                                )
 
 from rest_framework import routers
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView,
+                                            TokenVerifyView
+                                            )
 
 router = routers.SimpleRouter()
 
 router.register('project', ProjectViewSet, basename='project')
 router.register('issue', IssueViewSet, basename='issue')
 router.register('comment', CommentViewSet, basename='comment')
-router.register('admin/project', AdminProjectViewSet, basename='admin-project')
 router.register('staff/issue', StaffIssueViewSet, basename='staff-issue')
+router.register('user', UserListViewSet, basename='user')
+router.register('contributor', ContributorViewSet, basename='contributor')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', signup, name='signup'),
-    path('home/', homepage, name='home'),
-    path('project_creation/', project_creation_view, name='project_creation'),
-    path('issue_creation/', issue_creation_view, name='issue_creation'),
-    path('comment_creation/', comment_creation_view, name='comment_creation'),
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
